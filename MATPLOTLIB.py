3c3e41222239d2e799cd23ece8d5cc4a2d0b6eb8
devansh-208 <86621567+devansh-208@users.noreply.github.com>
2021-06-28 23:04:22 +0530

    Add files via upload

---
+++ MATPLOTLIB.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import matplotlib.pyplot as plt
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[8]:
+
+
+x=np.linspace(0,5,11)
+
+
+# In[9]:
+
+
+print(x)
+
+
+# In[10]:
+
+
+y=x**2
+
+
+# In[11]:
+
+
+print (y)
+
+
+# In[12]:
+
+
+plt.plot(x,y,'g')
+
+
+# In[13]:
+
+
+plt.plot(x,y,'b')
+plt.xlabel('X-AXIS')
+plt.ylabel('Y-AXIS')
+plt.title('MY TRIAL')
+plt.show()
+
+
+# In[15]:
+
+
+plt.subplot(1,2,1)
+plt.plot(x,y,'r-*')
+
+
+# In[16]:
+
+
+import math
+
+
+# In[ ]:
+
+
+ROP = float(input('Enter the ROP: '))
+RPM = float(input('Enter the RPM: '))
+WOB = float(input('Enter the WOB: '))
+B = float(input('Enter the bit diameter: '))
+x= ROP/60*RPM
+y= 12*WOB/10**6*B
+d = logx1 * logx2
+
+
+# c=8
+# while c<=10:
+#     print('hello brother')
+#     c = c+1
+
+# In[17]:
+
+
+import numpy as np
+
+
+# In[6]:
+
+
+i = 0
+for i in ROP:
+    if ROP%2 is 0:
+        print(ROP)
+    else:
+        print('LOL')
+
+
+# In[ ]:
+
+
+
+
